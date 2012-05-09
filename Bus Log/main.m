263877eb0f9bfcd965da64abffbea147f70dfd52
Yukiko Ishida <Yukiko@Yukiko-Ishidas-MacBook-Air.local>
2012-05-08 20:18:04 -0700

    Initial Commit

---
+++ Bus Log/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Bus Log
+//
+//  Created by Yukiko Ishida on 5/8/12.
+//  Copyright Yukiko Design 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
